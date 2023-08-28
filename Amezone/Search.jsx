8c8c98c9da331ff2d8ad797dd90448ea1c110885
kambi22 <kambikot8@gmail.com>
2023-08-28 18:34:25 +0530

    change in all

--- Amezone/Search.jsx
+++ Amezone/Search.jsx
@@ -1,6 +1,12 @@
-function Search(){
-    return(
-        <div>Search Page</div>
-    )
-}
+import React from "react"
+
+const Search = (props) => {
+  return (
+    <div>
+      <h5>this is a search pahge</h5>
+    </div>
+  )
+};
+
 export default Search;
+
